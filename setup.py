12c41a80c6556747d508563fda650fb2c7b2bbf9
yubie-re <24496817+yubie-re@users.noreply.github.com>
2025-01-17 21:38:15 -0500

    increment vers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='castleio',
-    version='0.1.10',
+    version='0.1.11',
     packages=['castleio'],
     install_requires=['pytz'],
     description='Generates castle.io tokens',
